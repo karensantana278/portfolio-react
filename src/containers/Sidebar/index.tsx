d106a0a6934f1f1bc2315b1b105b67d72cdd6d96
karensantana278 <karensantana278@gmail.com>
2024-02-05 21:12:34 -0300

    add components structure

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/karensantana278.png" alt="myllena karen" />
+    <Titulo>Myllena Lucena</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+      perferendis fugiat similique doloribus sint ipsa facere provident
+      voluptatibus aliquam tenetur quidem culpa fuga aperiam accusamus, eveniet,
+      alias nostrum tempore accusantium.
+    </p>
+  </section>
+)
+
+export default Sobre
